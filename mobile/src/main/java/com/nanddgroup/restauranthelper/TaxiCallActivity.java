f739738c6b1d669976ef345d603bbef52e4ba194
Nikita Romanenko <nikita241296@gmail.com>
2016-03-15 15:33:08 +0200

    taxi activity created

---
+++ mobile/src/main/java/com/nanddgroup/restauranthelper/TaxiCallActivity.java
@@ -0,0 +1,13 @@
+package com.nanddgroup.restauranthelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaxiCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taxi_call);
+    }
+}
