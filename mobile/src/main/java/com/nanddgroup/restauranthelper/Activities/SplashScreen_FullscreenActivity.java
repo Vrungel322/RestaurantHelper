4d75ba9fd911c909e2089a4ca396087daddd1e82
Nikita Romanenko <nikita241296@gmail.com>
2016-03-25 23:29:53 +0200

    SplashScreen - FullScreen fixed bug

--- mobile/src/main/java/com/nanddgroup/restauranthelper/Activities/SplashScreen_FullscreenActivity.java
+++ mobile/src/main/java/com/nanddgroup/restauranthelper/Activities/SplashScreen_FullscreenActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.nanddgroup.restauranthelper.R;
 
@@ -18,6 +20,9 @@ public class SplashScreen_FullscreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash_screen__fullscreen);
 
